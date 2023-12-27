d60da8225391a6d409ffc6dbde161547181e7956
derpyhooves <scottoss1@outlook.com>
2023-12-27 18:44:22 +0100

    Update processor.py

--- slashtags/mixins/processor.py
+++ slashtags/mixins/processor.py
@@ -268,33 +268,9 @@ class Processor(MixinMeta):
     ) -> Optional[discord.Message]:
         destination = interaction
 
-        if target := actions.get("target"):
-            if target == "dm":
-                destination = interaction.author
-                del kwargs["ephemeral"]
-            elif target != "reply":
-                try:
-                    chan = await self.channel_converter.convert(interaction, target)
-                except commands.BadArgument:
-                    pass
-                else:
-                    if chan.permissions_for(interaction.me).send_messages:
-                        destination = chan
-                        del kwargs["ephemeral"]
-
-        if not (content or embed is not None):
-            return
-
-        try:
-            return await destination.send(content, embed=embed, **kwargs)
-        except discord.HTTPException as exc:
-            log.exception(
-                "Error sending to destination:%r for interaction:%r\nkwargs:%r",
-                destination,
-                interaction,
-                kwargs,
-                exc_info=exc,
-            )
+        
+        return await destination.reply(content, embed=embed, **kwargs)
+        
 
     async def validate_checks(self, ctx: commands.Context, actions: dict):
         to_gather = []
